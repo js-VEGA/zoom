10ad1e4847504c8aed3ae86d218aa566cbe3d5bb
임진성 <imjinsung0319@gmail.com>
2022-03-29 15:28:03 +0900

    수정됨

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,6 +3,11 @@ const nickForm = document.querySelector("#nick");
 const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type,payload){
+    const msg = {type,payload};
+    return JSON.stringify(msg);
+}
+
 function handleOpen(){
     console.log("Connected to Server");
 }
@@ -22,14 +27,14 @@ socket.addEventListener("close",()=>{
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message",input.value));
     input.value = "";
   }
   
   function handleNickSubmit(event){
     event.preventDefault();
     const input = nickForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("nickname",input.value));
   }
 
   messageForm.addEventListener("submit", handleSubmit);
